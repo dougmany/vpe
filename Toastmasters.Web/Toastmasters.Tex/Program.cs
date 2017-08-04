3d93f15f99ae5fd3ee2931eb1b4c9ac81eb4b24f
dougmany <doug.meeker@arb.ca.gov>
2017-08-04 16:34:04 -0700

    Add Tex project.

---
+++ Toastmasters.Web/Toastmasters.Tex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Toastmasters.Tex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
