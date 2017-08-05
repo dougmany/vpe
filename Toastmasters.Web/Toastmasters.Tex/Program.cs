68039349b8c629fa4c9198a51f45e6ad0ac89e3b
Doug Meeker <wog400@hotmail.com>
2017-08-04 23:05:09 -0700

    Add file input and start parsing.

--- Toastmasters.Web/Toastmasters.Tex/Program.cs
+++ Toastmasters.Web/Toastmasters.Tex/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Toastmasters.Tex
 {
@@ -7,6 +9,53 @@ namespace Toastmasters.Tex
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var inpath = args[0];
+
+            if (String.IsNullOrEmpty(inpath))
+            {
+                throw new Exception("No input file given!");
+            }
+
+            string outpath = "";
+            if (String.IsNullOrEmpty(outpath))
+            {
+                outpath = inpath.Replace(".tex", "1.tex");
+                if (String.IsNullOrEmpty(outpath))
+                {
+                    outpath = String.Concat(inpath, "1");
+                }
+            }
+
+            using (var stream = new FileStream(inpath, FileMode.Open))
+            using (var reader = new StreamReader(stream))
+            {
+                try
+                {
+
+                    FileStream outStream = new System.IO.FileStream(outpath, FileMode.Create, FileAccess.Write);
+                    var outWriter = new StreamWriter(outStream, Encoding.UTF8);
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains("##"))
+                        {
+                            var start = line.IndexOf("##");
+                            var end = line.Substring(start).IndexOf("}");
+                            var variableName = line.Substring(start, end);
+                        }
+                    }
+                }
+                finally
+                {
+                    //if (outstream != null)
+                    //{
+                    //    outstream.Close();
+                    //    outstream = null;
+                    //}
+                }
+            }
         }
     }
 }
