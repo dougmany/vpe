dcc97e26aba916116a9f1d9089c223be6160ce8a
dougmany <wog400@hotmail.com>
2017-08-11 16:18:11 -0700

    Change server to xenial.

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "hashicorp/precise64"
+  config.vm.box = "boxcutter/ubuntu1604"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
@@ -75,5 +75,7 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
+  
   config.vm.provision :shell, path: "bootstrap.sh"
+  
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,12 @@ apt-get update
 echo -e "\n--- Install MySQL specific packages and settings ---\n"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOTPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOTPASSWD"
-apt-get -y install mysql-server python-mysqldb
+
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys B02C46DF417A0893
+apt-get update
+
+apt-get -y install mysql-server latex2rtf dotnet-dev-1.0.4
 
 echo -e "\n--- Seed Data ---\n"
 
@@ -25,39 +30,5 @@ INSERT INTO Members VALUES (4,'Gordon','Owyang','Gordon.Owyang@gartner.com',0,0)
 mysql -uroot -p$ROOTPASSWD -e "USE $DBNAME; INSERT INTO Meetings VALUES( 1, '2017-07-31 12:05:00', 4, 8, 13, 7, 10, 2, 1, 9, 12, 6, 15, 14, 1, 10, NULL, NULL );"
 mysql -uroot -p$ROOTPASSWD -e "USE $DBNAME; SELECT * FROM Members"
 
-echo -e "\n--- Install Latex ---\n"
-
-#echo 'selected_scheme scheme-meduim' > /vagrant/temp.profile
-#/vagrant/install-tl/install-tl -profile /vagrant/temp.profile
-#tlmgr install meetingmins
-
-# Download and unpack the latest TeX Live tarball
-wget -q http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz || error_exit "Failure getting texlive tarball"
-tar -xzf install-tl-unx.tar.gz || error_exit "Failure unpacking texlive tarball"
-rm -f install-tl-unx.tar.gz
-
-# Install with the meduim scheme
-cd install-tl-20* || error_exit "Failure changing to texlive install directory"
-echo 'selected_scheme scheme-meduim' > temp.profile
-./install-tl -profile temp.profile || error_exit "Failure installing texlive core"
-rm -f temp.profile
-
-# Add the bin directory to the path
-echo 'export PATH=/usr/local/texlive/2017/bin/x86_64-linux:$PATH' >> ~/.profile
-source ~/.profile
-
-install -o root -g root -m 755 -v /vagrant/latexdb-0.3/code/* /usr/local/bin/
-
-
- # full scheme (everything) 
- # medium scheme (small + more packages and languages)
- # small scheme (basic + xetex, metapost, a few languages) 
- # basic scheme (plain and latex)
- # minimal scheme (plain only)
- # ConTeXt scheme
- # GUST TeX Live scheme 
- # infrastructure-only scheme (no TeX at all)
- # teTeX scheme (more than medium, but nowhere near full) 
- # custom selection of collections
 
 
