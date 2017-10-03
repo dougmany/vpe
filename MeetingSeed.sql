b86b41f55ea8440f790bf977f400dcc819e78b9d
dougmany <wog400@hotmail.com>
2017-10-03 15:38:28 -0700

    Add meeting history.

--- MeetingSeed.sql
+++ MeetingSeed.sql
@@ -1,8 +1,15 @@
-INSERT INTO Meetings VALUES( 1, '2017-07-31 12:05:00',  4,  8, 13,  7, 10,  2,  1 , 9, 12,  6, 15, 14,  1, 10, NULL ,NULL );
-INSERT INTO Meetings VALUES( 2, '2017-08-28 12:05:00',  6,  3, 12, 15,  1,  5, 12 , 3,  9,  2,  7,  4,  2, 10,   17 ,    16 );
-INSERT INTO Meetings VALUES( 4, '2017-09-18 12:05:00',  7, 13, 10,  1, 15, 11,  6 , 2,  5, 14,  9,  8,  1, 10,   14 ,    14 );
-INSERT INTO Meetings VALUES( 5, '2017-09-25 12:05:00', 11,  7,  4,  5, 10,  3,  7 , 9, 14,  8, 14, 13,  1, 10,   14 ,    14 );
-INSERT INTO Meetings VALUES( 6, '2017-10-02 12:05:00', 14,  8,  1,  2,  9,  7, 12 , 4, 10, 13,  3, 12,  1, 10,   14 ,    14 );
-INSERT INTO Meetings VALUES( 7, '2017-10-09 12:05:00',  1, 12,  6,  3, 14, 11,  2 , 5, 13,  7,  4, 15,  1, 10,   14 ,    14 );
-INSERT INTO Meetings VALUES( 8, '2017-10-16 12:05:00',  5, 15, 14,  4, 12,  6, 13,  7,  1, 10,  2,  9,  1, 10, NULL, NULL );
-INSERT INTO Meetings VALUES( 9, '2017-10-23 12:05:00', 13, 14,  8, 11,  2,  4,  9,  1, 10, 15,  6,  5,  1, 10, NULL, NULL );
+INSERT INTO Meetings 
+(`MeetingID`, `MeetingDate`, `ToastmasterMemberID`, `InspirationalMemberID`, `JokeMemberID`, `GeneralEvaluatorMemberID`, `EvaluatorIMemberID`, `EvaluatorIIMemberID`, `TimerMemberID`, `BallotCounterMemberID`, `GrammarianMemberID`, `TableTopicsMemberID`, `SpeakerIMemberID`, `SpeakerIIMemberID`, `PresidentMemberID`, `SargentMemberID`, `AbsentIMemberID`, `AbsentIIMemberID`)
+VALUES
+( 1, '2017-07-31 12:05:00',  4,  8, 13,  7, 10,  2,  1 , 9, 12,  6, 15, 14,  1, 10, NULL ,NULL ),
+( 2, '2017-08-28 12:05:00',  6,  3, 12, 15,  1,  5, 12 , 3,  9,  2,  7,  4,  2, 10,   17 ,    16 ),
+( 4, '2017-09-18 12:05:00',  7, 13, 10,  1, 15, 11,  6 , 2,  5, 14,  9,  8,  1, 10,   14 ,    14 ),
+( 5, '2017-09-25 12:05:00', 11,  7,  4,  5, 10,  3,  7 , 9, 14,  8, 14, 13,  1, 10,   14 ,    14 ),
+( 6, '2017-10-02 12:05:00', 14,  8,  1,  2,  9,  7, 12 , 4, 10, 13,  3, 12,  1, 10,   14 ,    14 ),
+( 7, '2017-10-09 12:05:00',  1, 12,  6,  3, 14, 11,  2 , 5, 13,  7,  4, 15,  1, 10,   14 ,    14 ),
+( 8, '2017-10-16 12:05:00',  5, 15, 14,  4, 12,  6, 13,  7,  1, 10,  2,  9,  1, 10, NULL, NULL ),
+( 9, '2017-10-23 12:05:00', 13, 14,  8, 11,  2,  4,  9,  1, 10, 15,  6,  5,  1, 10, NULL, NULL ),
+(10, '2017-07-10 12:05:00',  9,  7,  4, 12, 11, 14,  4,  7, 10,  1,  3,  2,  1, 10, NULL, NULL ),
+(11, '2017-07-17 12:05:00',  2,  1,  7,  8,  6,  9, 10, 13, 14,  3,  4, 11,  1, 10, NULL, NULL ),
+(12, '2017-07-24 12:05:00', 15,  5, 11,  6,  7, 12, 11, 14,  2,  4, 13, 10,  1, 10, NULL, NULL ),
+(13, '2017-08-07 12:05:00',  8, 12,  3,  9,  4, 13,  5, 12,  2, 11,  1,  6,  1, 10, NULL, NULL );
