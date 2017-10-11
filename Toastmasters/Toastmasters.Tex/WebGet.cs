2eefd085188b1a6f572f3ee2a9a2e13c6a8b626a
dougmany <wog400@hotmail.com>
2017-10-11 16:27:35 -0700

    Add text email text to NextFive.  Change date format.

--- Toastmasters/Toastmasters.Tex/WebGet.cs
+++ Toastmasters/Toastmasters.Tex/WebGet.cs
@@ -180,6 +180,6 @@ namespace Toastmasters.Tex
         public String Sargent { get; set; }
         public String AbsentI { get; set; }
         public String AbsentII { get; set; }
-        public String MeetingDateString { get { return MeetingDate.ToString("f"); } }
+        public String MeetingDateString { get { return MeetingDate.ToString("M"); } }
     }
 }
