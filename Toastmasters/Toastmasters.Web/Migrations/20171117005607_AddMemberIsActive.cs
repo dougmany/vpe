4903b23e3e39d49da9ab2a471109a36d72119a5e
dougmany <wog400@hotmail.com>
2017-11-17 07:46:36 -0800

    Add Missing files

---
+++ Toastmasters/Toastmasters.Web/Migrations/20171117005607_AddMemberIsActive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Toastmasters.Web.Migrations
+{
+    public partial class AddMemberIsActive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Members",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Members");
+        }
+    }
+}
