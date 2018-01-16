86bf30fa2527c099d7c4a3b89ef246eeb7058871
Doug Meeker <wog400@hotmail.com>
2018-01-15 21:14:05 -0800

    Add missing files.

---
+++ Toastmasters/Toastmasters.Web/Services/EmailSender.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Toastmasters.Web.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailCredentials _credentials;
+
+        public EmailSender(IOptions<EmailCredentials> credentials)
+        {
+            _credentials = credentials.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("agwoow.ga", "dougmany@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect(_credentials.Server, 587, SecureSocketOptions.StartTls);
+                client.Authenticate(_credentials.Username, _credentials.Password);
+                await client.SendAsync(emailMessage);
+                client.Disconnect(true);
+
+            }
+        }
+    }
+
+    public class EmailCredentials
+    {
+        public String Server { get; set; }
+        public String Username{get;set; }
+        public String Password { get; set; }
+    }
+}
