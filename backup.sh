359c24d56cc952540e4e1eda188850a5e7b0ac3a
dougmany <wog400@hotmail.com>
2018-02-23 23:32:14 +0000

    Add following slash.  Add TrashTalkers to backup.

--- backup.sh
+++ backup.sh
@@ -1,2 +1,6 @@
-mysqldump -uroot -p toastmasters > /var/opt/aspnetcore/toastmasters.web/backup.sql
-aws s3 cp /var/opt/aspnetcore/toastmasters.web/backup.sql  s3://dougmany/backups
+mysqldump toastmasters > /var/opt/aspnetcore/toastmasters.web/backup.sql
+mysqldump toastmastersTraining > /var/opt/aspnetcore/toastmasters.web.training/backup.sql
+mysqldump toastmastersTrashTalkers > /var/opt/aspnetcore/toastmasters.trashtalkers.web/backup.sql
+
+aws s3 cp /var/opt/aspnetcore/toastmasters.web/backup.sql  s3://dougmany/backups/backup.sql
+aws s3 cp /var/opt/aspnetcore/toastmasters.trashtalkers.web/backup.sql  s3://dougmany/backups/trashtalkers.backup.sql
--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ sudo dotnet publish /vagrant/Toastmasters/Toastmasters.Web/ -c Release -o /var/a
 sed -i 's/5000/5002/g' /var/aspnetcore/toastmasters.carborators.web/hosting.json
 sed -i 's/database=toastmasters;/database=toastmastersCarborators;/g' /var/aspnetcore/toastmasters.carborators.web/appsettings.json
 sed -i 's/"CookieName": "toastmasters"/"CookieName": "toastmasterscarborators"/g' /var/aspnetcore/toastmasters.carborators.web/appsettings.json
-sed -i 's/\\home\\www-data\\log/\\home\\www-data\\log-carborators/g' /var/aspnetcore/toastmasters.carborators.web/nlog.config
+sed -i 's/\\home\\www-data\\log\\/\\home\\www-data\\log-carborators\\/g' /var/aspnetcore/toastmasters.carborators.web/nlog.config
 
 sudo systemctl stop kestrel-toastmasters-trashtalkers.service
 sudo dotnet publish /vagrant/Toastmasters/Toastmasters.Web/ -c Release -o /var/aspnetcore/toastmasters.trashtalkers.web/
@@ -20,7 +20,7 @@ sudo dotnet publish /vagrant/Toastmasters/Toastmasters.Web/ -c Release -o /var/a
 sed -i 's/5000/5003/g' /var/aspnetcore/toastmasters.trashtalkers.web/hosting.json
 sed -i 's/database=toastmasters;/database=toastmastersTrashTalkers;/g' /var/aspnetcore/toastmasters.trashtalkers.web/appsettings.json
 sed -i 's/"CookieName": "toastmasters"/"CookieName": "toastmasterstrashtalkers"/g' /var/aspnetcore/toastmasters.trashtalkers.web/appsettings.json
-sed -i 's/\\home\\www-data\\log/\\home\\www-data\\log-trashtalkers/g' /var/aspnetcore/toastmasters.trashtalkers.web/nlog.config
+sed -i 's/\\home\\www-data\\log\\/\\home\\www-data\\log-trashtalkers\\/g' /var/aspnetcore/toastmasters.trashtalkers.web/nlog.config
 
 sudo systemctl start kestrel-toastmasters.service
 sudo systemctl start kestrel-toastmasters-carborators.service
