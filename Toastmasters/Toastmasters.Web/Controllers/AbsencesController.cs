9ffa87f825cb5f6b1313109be4fa92dbc0333b2b
dougmany <wog400@hotmail.com>
2018-04-10 16:45:46 -0700

    Add Absences.

---
+++ Toastmasters/Toastmasters.Web/Controllers/AbsencesController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Toastmasters.Web.Data;
+using Toastmasters.Web.Models;
+
+namespace Toastmasters.Web.Controllers
+{
+    public class AbsencesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AbsencesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Absences
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Absences.ToListAsync());
+        }
+    }
+}
